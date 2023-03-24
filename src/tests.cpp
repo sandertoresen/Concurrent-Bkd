ab290441b876d22a1466a289d0d556dd9d671daa
sandertoresen <sandertoresen.com>
2023-03-24 11:24:06 -0400

    completed duplicate support test

--- src/tests.cpp
+++ src/tests.cpp
@@ -11,20 +11,18 @@ using namespace std;
 
 void _test_kdb_tree_memory_leak()
 {
-    int numNodes = 8;
+    int numNodes = 1024;
     DataNode *values = new DataNode[numNodes * DIMENSIONS];
     MockApi *API = new MockApi;
 
     int offset = 0;
     DataNode *value = new DataNode;
-    API->fetchRandom(value);
     for (int i = 0; i < numNodes; i++)
     {
-        // API->fetchRandom(&values[i]);
-        values[i].cordinates[0] = value->cordinates[0];
-        values[i].cordinates[1] = value->cordinates[1];
+        API->fetchRandom(&values[i]);
 
-        strcpy(values[i].location, value->location);
+        values[i].cordinates[0] = 1; // value->cordinates[0];
+        values[i].cordinates[1] = 2; // value->cordinates[1];
     }
 
     for (int d = 0; d < DIMENSIONS; d++)
@@ -40,10 +38,23 @@ void _test_kdb_tree_memory_leak()
     // Fetch nodes back
     DataNode *leakTest = new DataNode[numNodes];
     KdbTreeFetchNodes(tree, leakTest);
-    // for (int i = 0; i < numNodes; i++)
-    // {
-    //     printf("(%d,%d):%s\n", leakTest->cordinates[0], leakTest->cordinates[1], leakTest->location);
-    // }
+
+    for (int i = 0; i < numNodes * DIMENSIONS; i++)
+    {
+        bool found = false;
+        for (int j = 0; j < numNodes; j++)
+        {
+            if (__cordEqual(values[i].cordinates, leakTest[j].cordinates) && strcmp(values[i].location, leakTest[j].location) == 0)
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            printf("Didn't find one node!!\n");
+        }
+    }
 
     KdbDestroyTree(tree);
     printf("task exited succ\n");
