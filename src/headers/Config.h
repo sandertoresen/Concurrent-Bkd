643556bb1a67709048c33ea5e01dfae9b38c2148
sandertoresen <sandertoresen.com>
2023-04-19 00:01:50 +0200

    fixed large bulkloading, code close to complete

--- src/headers/Config.h
+++ src/headers/Config.h
@@ -9,30 +9,30 @@
 // it will also increase the communication between threads as there would be more writes to global memory per insert
 // however smaller buffers would cause readers to have more updated data.
 // should perform a test testing the effect of different thread buffer sizes.
-#define KDB_LEAF_SIZE 64
+#define KDB_LEAF_SIZE 128
 
-#define THREAD_BUFFER_SIZE 128
+#define THREAD_BUFFER_SIZE 1024
 // Chunk size should be a size so that a bulkload is faster than filling the entire memory
-#define GLOBAL_B_CHUNK_SIZE 4
+#define GLOBAL_B_CHUNK_SIZE 16
 #define GLOBAL_BUFFER_SIZE (GLOBAL_B_CHUNK_SIZE * THREAD_BUFFER_SIZE)
 
 // max (GLOBAL_BUFFER_SIZE * 2) * 5 size trees
 #define MAX_BULKLOAD_LEVEL 1
 #define SCHEDULER_MAP_ARRAY_SIZE 1024
 
-#define EPOCH_WAIT_NUM 20
+#define EPOCH_WAIT_NUM 10
 #define LARGE_BULKLOAD_SIZE 2
-#define LARGEST_BULKLOAD_CAP 512
+#define LARGEST_BULKLOAD_CAP 2048
 
 #define API_DELAY_MS 0
 
 #define INITIAL_WRITERS 3
-#define INITIAL_READERS 0
+#define INITIAL_READERS 1
 #define BULK_THREAD 1
 
 #define BLOOM_SIZE 1000000
 #define BLOOM_NUM_HASHES 6
 
-#define TREES_CREATED 10000
+#define TREES_CREATED 5000
 
 #endif
