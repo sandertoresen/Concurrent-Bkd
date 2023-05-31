38de85c37e2777f51d10abe00909fa09bda768c4
sandertoresen <sandertoresen.com>
2023-05-31 15:38:20 +0200

    added insert script test

---
+++ InsertTestScript.py
@@ -0,0 +1,42 @@
+import subprocess
+
+# Define the programs to run and their configurations
+programs = {
+    "program16x64": [1, 2, 3, 4, 5, 6, 7],
+    "program4x64": [1, 2, 3, 4, 5, 6, 7],
+    "program64x64": [1, 2, 3, 4, 5, 6, 7],
+    "program16x128": [1, 2, 3, 4, 5, 6, 7],
+    "program4x128": [1, 2, 3, 4, 5, 6, 7],
+    "program64x128": [1, 2, 3, 4, 5, 6, 7]
+}
+
+# Define the number of times to run each configuration
+num_runs = 10
+
+# Create a dictionary to store the results
+results = {}
+
+# Run the tests for each program
+for program, configurations in programs.items():
+    results[program] = {}
+    for config in configurations:
+        elapsed_times = []
+        for _ in range(num_runs):
+            # Run the program and capture the output
+            output = subprocess.check_output(
+                [f"./{program}", str(config)]).decode("utf-8")
+            # Extract the elapsed time from the output
+            elapsed_time = float(output.strip().split(" ")[2][:-1])
+            elapsed_times.append(elapsed_time)
+        # Calculate the average elapsed time
+        average_time = sum(elapsed_times) / num_runs
+        # Store the average in the results dictionary
+        results[program][config] = average_time
+        print(f"{program}: {config} - {average_time}")
+
+# Write the results to a file
+with open("results.txt", "w") as file:
+    for program, config_results in results.items():
+        file.write(f"----{program}------\n")
+        for config, average_time in config_results.items():
+            file.write(f"Average thread {config}: {average_time}s\n")
